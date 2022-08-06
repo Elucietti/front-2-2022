4b39c0ce584b8195b585e2880d917b14ddc363f8
Emanuel <emanuellucietti@tdf.edu.ar>
2022-08-06 14:01:21 -0300

    ejercicio-2-practica

--- ejercicio de practica-3/script.js
+++ ejercicio de practica-3/script.js
@@ -1,3 +1,17 @@
 "strict";
 
-let boton=document.getElementById("bnt");
+let boton=document.getElementById("btn");
+boton.addEventListener("click", mostrar);
+document.querySelector("div").classList.toggle("oculto");
+
+function mostrar(){
+    
+    
+    let miDiv=document.querySelector("div");
+    let crear=document.createElement("div");
+  
+     miDiv.appendChild(crear); 
+     document.querySelector("div").classList.toggle("oculto");
+
+    alert("Nuevo Div");
+}
