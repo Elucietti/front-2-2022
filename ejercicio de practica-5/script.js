815e51db121ff767467fed54f32a4c9c4a0e3638
Emanuel <emanuellucietti@tdf.edu.ar>
2022-08-09 19:20:30 -0300

    ejercicio-5-practica

--- ejercicio de practica-5/script.js
+++ ejercicio de practica-5/script.js
@@ -1,6 +1,7 @@
 "strict";
 
 
+alert("el nombre debe tener min 3 y max 20 caracteres");
 let vBoton=document.getElementById("btn-enviar");
 let vNombre=document.getElementById("nombre");
 let vApellido=document.getElementById("apellido");
@@ -26,73 +27,16 @@ function validar(nombre,apellido,mail,consulta){
 }
 
 
+/*
+function nombreValido(nombre){
+  if (document.fvalida.nombre.value.length==0){
+    alert("Tiene que escribir su nombre")
+    document.validar.nombre.focus()
+    return 0;
+}
 
-function nombreValido(nombre){}
+*/
 function apellidoValido(apellido){}
 function mailValido(mail){}
 function consultaValido(consulta){}
 
-
-
-/*
-
-/
-function emailValido(email) {
-  console.log(email)
-    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-}
-
-
-
-
-
-
-
-
-
-
-
-/*
-function sinValidar(e){
-
-
-
-}
-
-function validar(){
-
-        let nombre=document.getElementById("nombre");
-        let apellido=document.getElementById("apellido");
-        let mail=document.getElementById("mail");
-        let consulta=document.getElementById("consulta");
-
-    if(mail.validity.typeMismatch){
-        mail.setCustomValidity("ingrese su Mail!");
-
-      }  else{
-        mail.setCustomValidity("");
-      }
-
-}
-
-
-
-
-
-
-
-
-
-
-const email = document.getElementById("mail");
-
-email.addEventListener("input", function (event) {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("¡Se esperaba una dirección de correo electrónico!");
-  } else {
-    email.setCustomValidity("");
-  }
-});
-
-*/
