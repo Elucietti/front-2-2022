90bc36ecee3baf427976c0b4f42c710eed83fce9
Emanuel <emanuellucietti@tdf.edu.ar>
2022-08-12 18:46:56 -0300

    dados

---
+++ clase-15-(05-08)/script.js
@@ -0,0 +1,20 @@
+
+
+let cantidad7=0;
+
+let boton=document.getElementById("btn-lanzar");
+boton.addEventListener("click",lanzar);
+
+function lanzar(){
+
+        let dado1=0;
+        let dado2=0;
+
+        for(let i=0; i<1000;1++){
+
+            dado1=
+            dado2= 
+        }
+
+
+}
