70d16f749eebf11eec9d77b6a83016ba98b6dbd1
Emanuel <emanuellucietti@tdf.edu.ar>
2022-08-28 11:15:56 -0300

    trabajo supermercado

---
+++ clase-17/script.js
@@ -0,0 +1,9 @@
+
+
+let total=0;
+let btnCompra = document.getElementById("boton-comprar");
+btnCompra.addEventListener("click", funcionComprar);
+
+let productos=["Jugo","Mermelada","Fideos","leche"];
+let precioUnidad=[10,15,8,5];
+let stock=[15,10,10,9];
